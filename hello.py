34b7fdfd4366cf53220b3412a85057438e02d4f8
Will Minck <willminck@0587388849.wireless.umich.net>
2018-01-30 09:50:27 -0500

    Adding Will's personal statements

--- hello.py
+++ hello.py
@@ -1,2 +1,6 @@
 print("My name is Will")
 print("waddup minck")
+print("Favorite Food: Sandwiches")
+print("Favorite Music: Rap")
+print("Astronomical Sign: Taurus")
+print("Favorite Place: Florence, Italy")
